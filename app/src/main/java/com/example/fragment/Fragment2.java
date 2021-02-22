3573215c0994bfe49dafb0c7982b51a7afe68e11
Nahid2194 <nahidjc80@gmail.com>
2021-02-22 13:43:08 +0600

    completed

--- app/src/main/java/com/example/fragment/Fragment2.java
+++ app/src/main/java/com/example/fragment/Fragment2.java
@@ -9,13 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class Fragment1 extends Fragment {
-
+public class Fragment2 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        return inflater.inflate(R.layout.fragment_2, container, false);
     }
 }
--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonFunction(View view) {
+
+        Fragment myFragment;
+        if(view.getId()==R.id.fragment1){
+            myFragment = new Fragment1();
+        }else{
+            myFragment = new Fragment2();
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentAreaId,myFragment);
+        ft.commit();
     }
 }
